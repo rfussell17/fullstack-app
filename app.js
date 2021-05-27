3f3dbb88bdc994dacea7c47acea73b96da0c9519
Robin F <rfussell17@gmail.com>
2021-05-27 10:51:00 -0700

    add post route for new campgrounds

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 })
 
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save()
+    res.redirect(`/campgrounds/${campground.id}`)
+})
+
 app.get('/campgrounds/:id', async(req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show', { campground });
