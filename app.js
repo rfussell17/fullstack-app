81654d8dafc393284dc1cccbcd975fe3a3e5eb2c
Robin F <rfussell17@gmail.com>
2021-05-27 10:34:55 -0700

    add get route for campgrounds/show

--- app.js
+++ app.js
@@ -37,7 +37,8 @@ app.get('/campgrounds', async (req, res) => {
 
 
 app.get('/campgrounds/:id', async(req, res) => {
-    res.render('campgrounds/show')
+    const campground = await Campground.findById(req.params.id)
+    res.render('campgrounds/show', { campground });
 })
 
 
