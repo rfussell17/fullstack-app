c07a787150217852c228faa57c7d24910b679287
Robin F <rfussell17@gmail.com>
2021-05-27 11:11:57 -0700

    finish put route for edit submit in app.js

--- app.js
+++ app.js
@@ -56,7 +56,9 @@ app.get('/campgrounds/:id/edit', async(req, res) => {
 })
 
 app.put('/campgrounds/:id', async (req, res) => {
-    
+    const { id } = req.params; 
+    const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground})
+    res.redirect(`/campgrounds/${campground.id}`)
 })
 
 
