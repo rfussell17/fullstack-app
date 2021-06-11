db2581d4c472eee72607a49af648b1c56c315958
Robin F <rfussell17@gmail.com>
2021-06-10 18:29:54 -0700

    make flash success partial

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -27,6 +27,7 @@ router.get("/", catchAsync(async (req, res, next) => {
   router.post("/", validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
+    req.flash('success', 'Successfully made a new campground');
     res.redirect(`/campgrounds/${campground.id}`);
   }));
   
