ddc8dfb572879bc88035e1269737a309a7f0c537
Robin F <rfussell17@gmail.com>
2021-06-10 18:05:16 -0700

    change public dir to path.join

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 const db = mongoose.connection;
@@ -31,7 +32,7 @@ app.set("views", path.join(__dirname, "views"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use('/campgrounds', campgrounds)
 app.use('/campgrounds/:id/reviews', reviews)
