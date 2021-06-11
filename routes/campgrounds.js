58a2ef108e13e24c6c77535c24390143bac961e0
Robin F <rfussell17@gmail.com>
2021-06-10 17:33:58 -0700

    move required files from app to campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,7 +1,18 @@
 const express = require('express');
 const router = express.Router();
+const catchAsync = require('../helpers/catchAsync');
+const ExpressError = require('../helpers/ExpressError');
+const Campground = require("../models/campground");
 
-
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if(error){
+      const msg = error.details.map(el => el.message).join(',')
+      throw new ExpressError(msg, 400);
+    } else {
+      next();
+    }
+  }
 
 router.get("/", catchAsync(async (req, res, next) => {
     const campgrounds = await Campground.find({});
