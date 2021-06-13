f2c3faf641b47b1bbdf52040e044f88e3230e440
Robin F <rfussell17@gmail.com>
2021-06-13 11:33:14 -0700

    install passport, passport local, passport local mongoose

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -33,25 +33,33 @@ router.get("/", catchAsync(async (req, res, next) => {
   
   router.get("/:id", catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
+    if(!campground){
+      req.flash('error', 'Cannot find campgound');
+      return res.redirect('/campgrounds');
+    }
     res.render("campgrounds/show", { campground });
   }));
   
   router.get("/:id/edit", catchAsync(async (req, res, next) => {
     const campground = await Campground.findById(req.params.id);
+    if(!campground){
+      req.flash('error', 'Cannot find campgound');
+      return res.redirect('/campgrounds');
+    }
     res.render("campgrounds/edit", { campground });
   }));
   
-  router.put("/:id", validateCampground, catchAsync(async (req, res, next) => {
+  router.put("/:id", validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, {
-      ...req.body.campground,
-    });
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    req.flash('success', 'Successfully updated campground');
     res.redirect(`/campgrounds/${campground._id}`);
   }));
   
   router.delete("/:id", catchAsync(async (req, res, next) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
+    req.flash('success', 'Successfully deleted campground');
     res.redirect("/campgrounds");
   }));
 
--- routes/reviews.js
+++ routes/reviews.js
@@ -25,6 +25,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     campground.reviews.push(review);
     await review.save();
     await campground.save();
+    req.flash('success', 'Review posted');
     res.redirect(`/campgrounds/${campground._id}`);
   }))
   
@@ -32,6 +33,7 @@ router.post('/', validateReview, catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } })
     await Review.findByIdAndDelete(reviewId);
+    req.flash('success', 'Review deleted');
     res.redirect(`campgrounds/${id}`);
   }))
 
