75fc38075905db90c5fca8f560134d4407371030
Robin F <rfussell17@gmail.com>
2021-06-22 14:04:40 -0700

    add image to model

--- cloudinary/index.js
+++ cloudinary/index.js
@@ -6,3 +6,16 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_KEY,
     api_secret: process.env.CLOUDINARY_SECRET
 })
+
+const storage = new CloudinaryStorage({
+    cloudinary, 
+    params: {
+        folder: 'YelpCamp',
+        allowedFormats: ['jpeg', 'png', 'jpg']
+    }
+})
+
+module.exports = {
+    cloudinary,
+    storage
+}
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,14 +4,15 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const multer = require('multer');
-const upload = multer({ dest: 'uploads/' });
+const { storage } = require('../cloudinary');
+const upload = multer({ storage });
 
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
     // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
-    .post(upload.single('image'), (req, res) => {
+    .post(upload.array('image'), (req, res) => {
         res.send(req.body)
     })
 
